d532238791cf1a13f4ab7566928abfa0370a7049
teslaistra <danyefimoff@gmail.com>
2020-04-08 23:04:25 +0300

    done

---
+++ AdressSort/AdressSort/AdressSort.cpp
@@ -0,0 +1,62 @@
+﻿// AdressSort.cpp: определяет точку входа для приложения.
+//
+
+#include "AdressSort.h"
+#include <vector>
+using namespace std;
+
+int main()
+{
+	int arr[10] ;
+
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	for (size_t i = 0; i < n; i++)
+	{
+		arr[i] = rand()%200;
+	}
+	cout << "Generated unsorted array: " << endl;
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << " \n";
+
+	int max, min;
+	max = min = arr[0];
+
+	for (size_t i = 1; i < n; i++)
+	{
+		if (arr[i] > max) max = arr[i];
+		else if (arr[i] < min) min = arr[i];
+	}
+	vector<int> b;
+
+	for (size_t i = 0; i <= max; i++)
+	{
+		b.push_back(max + 1);
+	}
+	for (size_t i = 0; i < n; i++)
+	{
+		b.at(arr[i]) = arr[i];
+	}
+
+	int k = 0; 
+
+	for (size_t i = min; i <= max; i++)
+	{
+		if (b.at(i) != max+1)
+		{
+			arr[k] = b.at(i);
+			k++;
+		}
+	}
+
+	cout << "Sorted array is: " << endl;
+	for (size_t i = 0; i < n; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
